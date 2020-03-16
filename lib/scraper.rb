d8d84d2cba67201e807e1edddc84ff07b38fc2e4
Paul von Hagen <pvonhagen26@gmail.com>
2020-03-16 17:20:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
-
+require 'pry'
 require_relative './course.rb'
 
 class Scraper
@@ -17,7 +17,7 @@ class Scraper
   end
 
   def get_page
-
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 
 end
